d05eea22c7dc920b864a3f4731a67890d75cffd0
Kyle Banker <banker@users.noreply.github.com>
2023-04-28 08:29:57 -0600

    Preserve ENV variables when starting dd_agent

--- start_ddagent.sh
+++ start_ddagent.sh
@@ -8,4 +8,4 @@ chown dd-agent:dd-agent /etc/datadog-agent/datadog.yaml
 chown dd-agent:dd-agent /etc/datadog-agent/conf.d/prometheus.d/conf.yaml
 
 usermod --shell /bin/bash dd-agent
-exec su - dd-agent -c "/opt/datadog-agent/bin/agent/agent run -p /opt/datadog-agent/run/agent.pid"
+exec su -p dd-agent -c "/opt/datadog-agent/bin/agent/agent run -p /opt/datadog-agent/run/agent.pid"
